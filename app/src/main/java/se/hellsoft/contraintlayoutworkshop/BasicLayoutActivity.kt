b790cb53ce09c348e04cfaca1e1008f899353de7
Erik Hellman <erik.hellman@hellsoft.se>
2018-02-28 07:54:02 +0100

    First commit

---
+++ app/src/main/java/se/hellsoft/contraintlayoutworkshop/BasicLayoutActivity.kt
@@ -0,0 +1,12 @@
+package se.hellsoft.contraintlayoutworkshop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BasicLayoutActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_basic_layout_actvity)
+  }
+}
